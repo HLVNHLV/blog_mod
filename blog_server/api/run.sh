d51604704f08a8504fa6f19372b1219cfeadf8b7
Anibal <57480914+HLVNHLV@users.noreply.github.com>
2022-10-11 13:55:40 +0200

    delete server

--- blog_server/api/run.sh
+++
@@ -1,3 +0,0 @@
-#ip=$(hostname -i | xargs)
-#php -S $ip:8000
-php -S 127.0.0.1:8000
--- blog_server/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$routes = [
-    'list'      => 'list.php',
-    'post'      => 'post.php',
-    'delete'    => 'delete.php'
-];
